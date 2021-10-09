1d24218e32a7986525cb10ce67dda4970f0a2501
gonjavi <javiercaliescali@gmail.com>
2021-10-09 17:26:01 -0500

    -

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp1.Controllers
+{
+    [Route(" /[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts() {
+            return "OK";
+        }
+    }
+}
---
+++ Models/ShopContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp1.Models
+{
+    public class ShopContext : DbContext
+    {
+
+
+        // para traer todos los datos de la base de datos
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
